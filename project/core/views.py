3805ea7f024ca0166d0404da9a50c4ea640493e7
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-12-20 22:21:27 -0800

    Local: added new views and routes for loading user list, refactored /api endpoint to return the user dictionary object after form submission.

--- project/core/views.py
+++ project/core/views.py
@@ -1,8 +1,7 @@
 # project/core/views.py
-from flask import render_template, Blueprint, request, redirect, url_for, jsonify
+from flask import render_template, Blueprint, request, jsonify
 from project.models.models import User
 from project import db
-import json
 
 core = Blueprint('core', __name__)
 
@@ -41,6 +40,23 @@ def username_validation():
 				})
 
 
+def convert(obj):
+	lst = []
+	if type(obj) == list:
+		for i in range(0, len(obj)):
+			lst.append(obj[i].as_dict())
+	else:
+		lst.append(obj.as_dict())
+	return lst
+
+def get_user(userName):
+	user = User.query.filter_by(username=userName).one_or_none()
+	if user is not None:
+		return convert(user)
+	else:
+		return None
+
+
 @core.route('/api', methods=['GET', 'POST'])
 def api():
 	form_data = request.get_json()
@@ -55,17 +71,24 @@ def api():
 		)
 		db.session.add(user)
 		db.session.commit()
-		return jsonify({ 'process': 'success' })
+		
+		getUser = get_user(username)
+		
+		if getUser is not None:
+			return jsonify({
+				'process': 'success',
+				'id': getUser[0]['id'],
+				'userName': getUser[0]['userName'],
+				'firstName': getUser[0]['firstName'],
+				'lastName': getUser[0]['lastName']
+			})
 	
 	return jsonify({ 'process': 'failed' })
-
-
-def convert(lst):
-	result = {lst[i]: lst[i+1] for i in range(0, len(lst), 2)}
 
 
 @core.route('/users', methods=['GET'])
 def users():
 	query = User.query.all()
-	
-	return "Test"
+	if len(query) != 0:
+		result = convert(query)
+		return jsonify(result)
