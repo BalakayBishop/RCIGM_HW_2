0b036ce65afb83f8570f0bdaf0389102744963bd
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-12-20 16:17:34 -0800

    Local: added new ajax for username validation and form submission to endpoints in views.py

--- project/core/views.py
+++ project/core/views.py
@@ -2,13 +2,13 @@
 from flask import render_template, Blueprint, request, redirect, url_for, jsonify
 from project.models.models import User
 from project import db
+import json
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-	user = User.query.all()
-	return render_template('index.html', user=user)
+	return render_template('index.html')
 
 
 def is_valid(username_check):
@@ -26,20 +26,46 @@ def username_validation():
 	if request.is_json:
 		if request.method == 'POST':
 			existingUser = is_valid(passedUsername)
-			if existingUser:
-				return jsonify({'border': 'solid 2px #5cd65c'})
+			if len(passedUsername) != 0:
+				if existingUser:
+					return jsonify({
+						'class': 'success'
+					})
+				else:
+					return jsonify({
+						'class': 'fail'
+					})
 			else:
-				return jsonify({'border': 'solid 2px #f11b2b'})
+				return jsonify({
+					'class': 'none'
+				})
 
 
 @core.route('/api', methods=['GET', 'POST'])
 def api():
-	form_data = request.form
-	user = User(
-		first_name=form_data['firstName'],
-		last_name=form_data['lastName'],
-		username=form_data['userName']
-	)
-	db.session.add(user)
-	db.session.commit()
-	return redirect(url_for('core.index'))
+	form_data = request.get_json()
+	first_name = form_data['firstName'],
+	last_name = form_data['lastName'],
+	username = form_data['userName']
+	if len(first_name)!= 0 and len(last_name) != 0 and len(username) != 0:
+		user = User(
+			first_name=first_name,
+			last_name=last_name,
+			username=username
+		)
+		db.session.add(user)
+		db.session.commit()
+		return jsonify({ 'process': 'success' })
+	
+	return jsonify({ 'process': 'failed' })
+
+
+def convert(lst):
+	result = {lst[i]: lst[i+1] for i in range(0, len(lst), 2)}
+
+
+@core.route('/users', methods=['GET'])
+def users():
+	query = User.query.all()
+	
+	return "Test"
--- project/static/main.js
+++ project/static/main.js
@@ -1,17 +1,91 @@
 $(document).ready(function() {
-	$('.username').on('keyup', function() {
+	
+	$.ajax({
+		url: '/users',
+		type:'GET',
+		success: function(response) {
+			console.log(response)
+		},
+		error: function(error) {
+			console.log(error)
+		}
+	});
+	
+	$('.username').on('input', function() {
 		let username = $(this).val()
 		$.ajax({
 			url: '/username_validation',
-			type: 'post',
+			type: 'POST',
 			contentType: 'application/json',
 			data: JSON.stringify({
 				input: username
 			}),
 			success: function(response) {
-				$('.username').css(response)
+				let usernameInput = document.querySelector('.username')
+				let taken = document.querySelector('#taken')
+				let available = document.querySelector('#available')
+				
+				if (response['class'] === 'success') {
+					usernameInput.classList.remove('fail')
+					usernameInput.classList.add('success')
+					$('#submitButton').prop('disabled', false)
+					available.style.display = 'block'
+					taken.style.display = 'none'
+				}
+				else if (response['class'] === 'fail') {
+					usernameInput.classList.remove('success')
+					usernameInput.classList.add('fail')
+					$('#submitButton').prop('disabled', true)
+					available.style.display = 'none'
+					taken.style.display = 'block'
+				}
+				else if (response['class'] === 'none') {
+					usernameInput.classList.remove('success')
+					usernameInput.classList.remove('fail')
+					$('#submitButton').prop('disabled', false)
+					available.style.display = 'none'
+					taken.style.display = 'none'
+				}
+			}
+		})
+	});
+	
+	$('#submitButton').on('click', function(event) {
+		event.preventDefault()
+		let successAlert = document.getElementById('successAlert')
+		let failAlert = document.getElementById('failAlert')
+		let firstName = document.querySelector('#firstName')
+		let lastName = document.querySelector('#lastName')
+		let userName = document.querySelector('#userName')
+		$.ajax({
+			url: '/api',
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify({
+				firstName: $('#firstName').val(),
+				lastName: $('#lastName').val(),
+				userName: $('#userName').val()
+			}),
+			success: function(response) {
+				if (response['process'] === 'success') {
+					successAlert.style.display = 'flex'
+					failAlert.style.display = 'none'
+					firstName.textContent = ''
+					lastName.textContent = ''
+					userName.textContent = ''
+				}
+				else if (response['process'] === 'fail') {
+					successAlert.style.display = 'none'
+					failAlert.style.display = 'flex'
+					firstName.textContent = ''
+					lastName.textContent = ''
+					userName.textContent = ''
+				}
 			}
 		})
 	});
 })
 
+// <li class="list-group-item">
+//
+// </li>
