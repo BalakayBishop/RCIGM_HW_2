309d8f2fe114bb2c0a5edc1423d907306b4d57ee
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2023-01-10 16:01:18 -0800

    Local: changes made to models.py to reflect ON DELETE CASCADE in MySQL for deleting a user will delete all files associated in the files table

--- project/models/models.py
+++ project/models/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
 	last_name = db.Column(db.String(50))
 	username = db.Column(db.String(50), unique=True)
 	
-	files = db.relationship('UserFiles', backref='files', lazy='dynamic')
+	files = db.relationship('UserFiles', backref='files', lazy='dynamic', cascade='all, delete')
 	
 	def __init__(self, first_name, last_name, username):
 		self.first_name = first_name
@@ -29,7 +29,7 @@ class UserFiles(db.Model):
 	__tablename__ = 'files'
 	
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
 	file_path = db.Column(db.String(255))
 	
 	def __init__(self, user_id, file_path):
